438144b4fbcd7289768c62ec8c1617aa7f1ce97f
Beltrán José Aceves Gil <beltran.aceves@udc.es>
2021-04-19 02:57:26 +0200

    GameServer 90% (bugs)

--- lib/todo/game_server.ex
+++ lib/todo/game_server.ex
@@ -13,6 +13,10 @@ defmodule Todo.GameServer do
   def answer(question, history) do
     GenServer.call(__MODULE__, {:answer, {question, history}})
   end
+  
+  def check_previous(question, history) do
+    GenServer.call(__MODULE__, {:check_previous, {question, history}})
+  end
 
   # Server
 
@@ -22,26 +26,47 @@ defmodule Todo.GameServer do
 
   def handle_call({:first}, _from, state) when state != nil do
     word = Todo.Repository.select_words() |> Enum.random()
-    {:reply, {[], word}, state}
+    {:reply, {[word], word}, state}
   end
 
-  def handle_call({:answer, {question, history}}, _from, state) when state != nil do
-    answer = Todo.Repository.select_words()
+  def handle_call({:answer, {question, history}}, _from, state) do
+    answer =
+      Todo.Repository.select_words()
       |> remove_used(history)
-      |> findAnswer(question)
-    
-    {:reply, {history++answer, answer}, state}
+      |> find_answer(question)
+      |> Enum.random()
+
+    {:reply, {[answer]++[question]++history, answer}, state}
   end
 
-  def remove_used(word_list, history) do
-    word_list 
-        |> Enum.filter(
-          fn word -> 
-            not(word in history)
-          end
-        )
+  def handle_call({:check_previous, {question, history}}, _from, state) do
+    correction = check_previous_aux(question, history)
+
+    {:reply, correction, state}
   end
 
-  defp findAnswer(_word_list, _question) do
+  def check_previous_aux(question, history) do
+    previous_question = history |> Enum.at(0)
+    previous_question_last_char = previous_question  |> String.at(String.length(previous_question) - 1)
+    question_first_char = question |> String.at(0)
+    case  previous_question_last_char == question_first_char do
+      true -> "correct"
+      false -> "invalid_response"
+    end
+  end
+
+  defp remove_used(word_list, history) do
+    word_list
+    |> Enum.filter(fn word ->
+      not (word in history)
+    end)
+  end
+
+  # TODO arreglar bug: como hace comparacion directa no funciona bien con las tildes (purgar todas las tildes??)
+  defp find_answer(word_list, question) do
+    word_list
+    |> Enum.filter(fn word ->
+      question |> String.at(String.length(question) - 1) == word |> String.at(0)
+    end)
   end
 end
--- lib/todo/router.ex
+++ lib/todo/router.ex
@@ -2,7 +2,7 @@ defmodule Todo.Router do
   use Plug.Router
   require EEx
 
-  #alias Todo.Server TODO: usar alias para todos los modulos con prefijo Todo.
+  # alias Todo.Server TODO: usar alias para todos los modulos con prefijo Todo.
 
   @template "priv/static/template.html.eex"
   @template_login "priv/static/template_login.html.eex"
@@ -54,14 +54,17 @@ defmodule Todo.Router do
           |> Map.put("siguiente", siguiente)
           |> build_response
       end
-      send_resp(conn, 200, built_response)
+
+    send_resp(conn, 200, built_response)
   end
 
   post "/respuesta" do
     response =
-      read_login(conn)
+      read_question(conn)
       # Por que aqui tengo que usar el prefijo Todo? Y no cuando llamo solo a Server?
       |> Todo.LoginServer.check_login()
+
+    IO.inspect(response)
 
     built_response =
       case response["state"] do
@@ -78,11 +81,19 @@ defmodule Todo.Router do
           |> build_error
 
         "ok" ->
-          {historial, siguiente} = Todo.GameServer.first()
+          {{historial, siguiente}, error}=
+          case Todo.GameServer.check_previous(response.question, response.history) do
+            "invalid_response" ->
+              {{response.history, response.history |> Enum.at(0)}, "invalid_response"}
+            "correct" ->
+              {Todo.GameServer.answer(response.question, response.history), ""}
+          end
+          
 
           response
           |> Map.put("historial", historial)
           |> Map.put("siguiente", siguiente)
+          |> Map.put("error", error)
           |> build_response
       end
 
@@ -91,10 +102,10 @@ defmodule Todo.Router do
 
   match(_, do: send_resp(conn, 404, "This is not the page you are looking for"))
 
-
   defp read_login(conn) do
     {:ok, body, _conn} = read_body(conn)
     split_body = String.split(body, "&")
+
     "username=" <> username =
       Enum.at(split_body, 0)
       |> String.replace("+", " ")
@@ -106,18 +117,62 @@ defmodule Todo.Router do
     %{username: username, pwd: pwd}
   end
 
+  defp read_question(conn) do
+    {:ok, body, _conn} = read_body(conn)
+    split_body = String.split(body, "&")
+    IO.inspect(body)
+    IO.inspect(split_body)
+
+    "username=" <> username =
+      Enum.at(split_body, 0)
+      |> String.replace("+", " ")
+
+    "pwd=" <> pwd =
+      Enum.at(split_body, 1)
+      |> String.replace("+", " ")
+
+    "question=" <> question =
+      Enum.at(split_body, 2)
+      |> String.replace("+", " ")
+
+    history_list =
+      if split_body
+         |> length() < 4 do
+        []
+      else
+        {_, list} =
+          split_body
+          |> List.pop_at(0)
+
+        {_, list} =
+          list
+          |> List.pop_at(0)
+
+        {_, list} =
+          list
+          |> List.pop_at(0)
+        list
+        |> Enum.map(fn entry ->
+          Enum.at(String.split(entry, "="), 1)
+        end)
+      end
+
+    %{username: username, pwd: pwd, question: question, history: history_list}
+  end
+
   defp build_error(response) do
     EEx.eval_file(@template_login, todos: [], error: response["state"])
   end
 
   defp build_response(response) do
-    IO.inspect response
     EEx.eval_file(@template,
       historial: response["historial"],
       siguiente: response["siguiente"],
       credenciales: [
-        response.username, response.pwd
-      ]
+        response.username,
+        response.pwd
+      ],
+      error: response["error"]
     )
   end
 end
